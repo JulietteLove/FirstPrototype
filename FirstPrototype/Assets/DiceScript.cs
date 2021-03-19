a6a104ad15feca024098eeeba7b24783bb35c34b
JulietteLove <80416448+JulietteLove@users.noreply.github.com>
2021-03-19 14:46:39 +0200

    D6 Button

---
+++ FirstPrototype/Assets/DiceScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceScript : MonoBehaviour
+{
+    private float NumberRolled;
+    public bool ButtonPressed = false;
+    public GameObject DiceText;
+
+    public void ButtonClicked()
+    {
+        ButtonPressed = true;
+
+        if (ButtonPressed == true)
+        {
+            NumberRolled = Random.Range(1f, 6f);
+            DiceText.GetComponent<UnityEngine.UI.Text>().text = NumberRolled.ToString("F0");
+            ButtonPressed = false;
+        }
+    }
+}
