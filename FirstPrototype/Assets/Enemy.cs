eb12e82e408a239b2ca6ba4549b4b9ad346c282e
JulietteLove <80416448+JulietteLove@users.noreply.github.com>
2021-03-22 22:42:37 +0200

    Revert "Revert "Combat System Implementation 1""
    This reverts commit 6f508232f00081807cec556c1d08ef7145909480.

---
+++ FirstPrototype/Assets/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string enemyName;
+    public int damage;
+    public int maxHealth;
+    public int currentHealth;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
